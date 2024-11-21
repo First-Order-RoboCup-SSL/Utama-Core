061cee4e366ecf9443ae9f91f71573118a0bba6c
Fred Huang <fredhuang122106@gmail.com>
2024-11-21 18:31:18 +0000

    updated named tuples

--- entities/data/command.py
+++ entities/data/command.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 
 RobotSimCommand = namedtuple(
     "RobotSimCommand",
-    ["local_forward_vel", "local_left_vel", "angular_vel", "kick_vel", "dribbler_vel"],
+    ["local_forward_vel", "local_left_vel", "angular_vel", "kick_spd", "kick_angle", "dribbler_spd"],
 )
 RobotInfo = namedtuple(
     "RobotInfo",
