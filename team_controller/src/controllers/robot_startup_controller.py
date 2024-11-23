35e5b450c56731ecd6a3196e39055b6cccb1ef8b
Fred Huang <fredhuang122106@gmail.com>
2024-11-23 01:34:48 +0000

    Updates for using named tuples

--- team_controller/src/controllers/robot_startup_controller.py
+++ team_controller/src/controllers/robot_startup_controller.py
@@ -1,10 +1,11 @@
 import time
 import threading
 import numpy as np
-from typing import Tuple, Optional, Dict, Union
+from typing import Tuple, Optional, Dict, Union, List
 
 from global_utils.math_utils import rotate_vector
 from entities.data.command import RobotSimCommand
+from entities.data.vision import RobotData, BallData
 from team_controller.src.data.vision_receiver import VisionDataReceiver
 from team_controller.src.controllers.sim_robot_controller import SimRobotController
 from motion_planning.src.pid.pid import PID
@@ -62,64 +63,75 @@ class StartUpController:
             balls = self.vision_receiver.get_ball_pos()
         return robots, balls
 
-    def _calculate_robot_velocities(
-            self,
-            robot_id: int,
-            target_coords: Union[Tuple[float, float], Tuple[float, float, float]],
-            robots: Dict[int, Optional[Tuple[float, float, float]]],
-            balls: Dict[int, Tuple[float, float, float]],
-            face_ball=False,
-        ) -> RobotSimCommand:
-            """
-                    Calculates the linear and angular velocities required for a robot to move towards a specified target position
-                    and orientation.
+def _calculate_robot_velocities(
+        self,
+        robot_id: int,
+        target_coords: Union[Tuple[float, float], Tuple[float, float, float]],
+        robots: List[RobotData],
+        balls: List[BallData],
+        face_ball=False,
+    ) -> RobotSimCommand:
+        """
+                Calculates the linear and angular velocities required for a robot to move towards a specified target position
+                and orientation.
 
-                    Args:
-                        robot_id (int): The identifier of the robot.
-                        target_coords (Tuple[float, float] or Tuple[float, float, float]): The target position and orientation.
-                        robots (Dict[int, Optional[Tuple[float, float, float]]]): A dictionary containing the positions of all robots.
-                        balls (Dict[int, Tuple[float, float, float]]): A dictionary containing the positions of all balls.
-                        face_ball (bool, optional): Whether the robot should face the ball. Defaults to False.
+                Args:
+                    robot_id (int): Unique identifier for the robot.
+                    target_coords (Tuple[float, float] | Tuple[float, float, float]): Target coordinates the robot should move towards.
+                        Can be a (x, y) or (x, y, orientation) tuple. If `face_ball` is True, the robot will face the ball instead of
+                        using the orientation value in target_coords.
+                    robots (Dict[int, Optional[Tuple[float, float, float]]]): All the Current coordinates of the robots sepateated
+                        by thier robot_id which containts a tuple (x, y, orientation).
+                    balls (Dict[int, Tuple[float, float, float]]): All the Coordinates of the detected balls (int) , typically (x, y, z/height in 3D space).            face_ball (bool, optional): If True, the robot will orient itself to face the ball's position. Defaults to False.
 
-                    Returns:
-                        RobotSimCommand: A named tuple containing the linear and angular velocities, kick speed, kick angle, and dribbler speed.
-                        
-                    The method uses PID controllers to calculate velocities for linear and angular movement. If `face_ball` is set,
-                    the robot will calculate the angular velocity to face the ball. The resulting x and y velocities are rotated to align
-                    with the robot's current orientation.
-            """
-            
-            # Get current positions
-            if balls[0] and robots[robot_id]:
-                ball_x, ball_y, ball_z = balls[0]
-                current_x, current_y, current_oren = robots[robot_id]
+                Returns:
+                    Dict[str, float]: A dictionary containing the following velocity components:
+                        - "id" (int): Robot identifier.
+                        - "xvel" (float): X-axis velocity to move towards the target.
+                        - "yvel" (float): Y-axis velocity to move towards the target.
+                        - "wvel" (float): Angular velocity to adjust the robot's orientation.
 
-            target_x, target_y = target_coords[:2]
+                The method uses PID controllers to calculate velocities for linear and angular movement. If `face_ball` is set,
+                the robot will calculate the angular velocity to face the ball. The resulting x and y velocities are rotated to align
+                with the robot's current orientation.
+        """
+        
+        # Get current positions
+        if balls[0] and robots[robot_id]:
+            ball_x, ball_y, ball_z = balls[0]
+            current_x, current_y, current_oren = robots[robot_id]
 
-            if face_ball:
-                target_oren = np.atan2(ball_y - current_y, ball_x - current_x)
-            elif not face_ball and len(target_coords) == 3:
-                target_oren = target_coords[2]
+        target_x, target_y = target_coords[:2]
 
-            # print(f"\nRobot {robot_id} current position: ({current_x:.3f}, {current_y:.3f}, {current_oren:.3f})")
-            # print(f"Robot {robot_id} target position: ({target_x:.3f}, {target_y:.3f}, {target_oren:.3f})")
+        if face_ball:
+            target_oren = np.atan2(ball_y - current_y, ball_x - current_x)
+        elif not face_ball and len(target_coords) == 3:
+            target_oren = target_coords[2]
 
-            if target_oren != None:
-                angular_vel = self.pid_oren.calculate(
-                    target_oren, current_oren, robot_id, oren=True, normalize_range=np.pi/2
-                )
+        # print(f"\nRobot {robot_id} current position: ({current_x:.3f}, {current_y:.3f}, {current_oren:.3f})")
+        # print(f"Robot {robot_id} target position: ({target_x:.3f}, {target_y:.3f}, {target_oren:.3f})")
 
-            if target_x != None and target_y != None:
-                left_vel = self.pid_trans.calculate(
-                    target_y, current_y, robot_id, normalize_range=3000
-                )
-                forward_vel = self.pid_trans.calculate(
-                    target_x, current_x, robot_id, normalize_range=4500
-                )
+        if target_oren != None:
+            angular_vel = self.pid_oren.calculate(
+                target_oren, current_oren, robot_id, oren=True
+            )
+        else:
+            angular_vel = 0
 
-                forward_vel, left_vel = rotate_vector(
-                    forward_vel, left_vel, current_oren
-                )
+        if target_x != None and target_y != None:
+            left_vel = self.pid_trans.calculate(
+                target_y, current_y, robot_id, normalize_range=3000
+            )
+            forward_vel = self.pid_trans.calculate(
+                target_x, current_x, robot_id, normalize_range=4500
+            )
 
-            # print(f"Output: {forward_vel}, {left_vel}, {angular_vel}")
-            return RobotSimCommand(local_forward_vel=forward_vel, local_left_vel=left_vel, angular_vel=angular_vel, kick_spd=0, kick_angle=0, dribbler_spd=0)
+            forward_vel, left_vel = rotate_vector(
+                forward_vel, left_vel, current_oren
+            )
+        else:
+            forward_vel = 0
+            left_vel = 0
+
+        # print(f"Output: {forward_vel}, {left_vel}, {angular_vel}")
+        return RobotSimCommand(local_forward_vel=forward_vel, local_left_vel=left_vel, angular_vel=angular_vel, kick_spd=0, kick_angle=0, dribbler_spd=0)
