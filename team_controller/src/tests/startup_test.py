7d12f4c7cff1d37a65e5d5561427389dada919e7
Fred Huang <fredhuang122106@gmail.com>
2024-12-04 21:14:05 +0000

    hot fixes

--- team_controller/src/tests/startup_test.py
+++ team_controller/src/tests/startup_test.py
@@ -5,13 +5,12 @@ import queue
 from entities.game import Game
 import time
 
-from team_controller.src.data.message_enum import MessageType
-
 # Add the project root directory to sys.path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
 print(project_root)
 sys.path.insert(0, project_root)
 
+from team_controller.src.data.message_enum import MessageType
 from team_controller.src.data import VisionDataReceiver
 from team_controller.src.controllers.sim.robot_startup_controller import (
     StartUpController,
