ac651d665f7adac909a6a03e018af1c4debf9ec3
fred-huang122 <fredhuang122106@gmail.com>
2024-12-20 17:57:11 +0800

    [UNTESTED] initial implementation of roles with robot + added sim controller integration

--- entities/game/robot.py
+++ entities/game/robot.py
@@ -1,41 +1,53 @@
-## NOT CURRENTLY USED ###
+from typing import Tuple, Optional, List, Union
+from dataclasses import dataclass, field
+import numpy as np
 
-from typing import Tuple
+from entities.game.role import Role
+from team_controller.src.controllers import GRSimController
+from team_controller.src.controllers import RSimController
 
+ROLES = ["attack", "defend", "keeper", "ball_placement"]
 
+@dataclass
 class Robot:
-    def __init__(self, robot_id, team_colour):
-        self._id = robot_id
-        self._team_colour = team_colour
-        self._has_ball = False
-        self._pos = None
-        self._heading = None
-        self._records = []
+    robot_id: int
+    is_team_yellow: bool
+    role: Role = field(init=False)
+    has_ball: bool = False
+    pos: Optional[Tuple[float, float]] = None
+    theta: Optional[float] = None
+    agro_rating: int = 0
+    records: List[str] = field(default_factory=list)
+    using_grsim: bool = True
+    sim_controller = GRSimController if using_grsim else RSimController
 
-    @property
-    def id(self) -> int:
-        return self._id
+    def __post_init__(self):
+        self.role = Role(self.robot_id)
 
-    @property
-    def team_colour(self) -> str:
-        return self._team_colour
+    def change_role(self, role: Union[int, str]) -> None:
+        self.role.change_role(role)
 
-    @property
-    def has_ball(self) -> bool:
-        return self._has_ball
+    def set_position(self, x: float, y: float) -> None:
+        if x < 0 or y < 0:
+            raise ValueError("Position coordinates must be non-negative")
+        self.pos = (x, y)
+        self.sim_controller.teleport_robot(
+        is_team_yellow=self.is_team_yellow, robot_id=self.robot_id, x=x, y=y
+        )
 
-    @property
-    def pos(self) -> Tuple[float, float]:
-        return self._pos
+    def set_theta(self, theta: float) -> None:
+        if not (-np.pi < theta <= np.pi):
+            raise ValueError("Heading must be in range (-pi, pi]")
+        self.heading = theta
+        self.sim_controller.teleport_robot(
+        is_team_yellow=self.is_team_yellow, robot_id=self.robot_id,theta=self.theta
+        )
 
-    @property
-    def x(self) -> float:
-        return self._pos[0]
-
-    @property
-    def y(self) -> float:
-        return self._pos[1]
-
-    @property
-    def heading(self) -> float:
-        return self._heading
+    def update_has_ball(self, has_ball: bool) -> None:
+        self.has_ball = has_ball
+        
+    def update_agro_rating(self, agro_rating: float):
+        if agro_rating >= 0 :
+            self.agro_rating = agro_rating
+        else:
+            raise ValueError("agro raiting value must be non-negative")
