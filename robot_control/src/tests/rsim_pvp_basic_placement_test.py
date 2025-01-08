57120b12da32aa13ab8edccacadb2951070dc589
Hamish <hamishstarling@hotmail.co.uk>
2025-01-08 17:08:41 +0000

    Move utils out into utils file

--- robot_control/src/tests/rsim_pvp_basic_placement_test.py
+++ robot_control/src/tests/rsim_pvp_basic_placement_test.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 from motion_planning.src.pid.pid import TwoDPID
 from robot_control.src.skills import go_to_ball, go_to_point
+from robot_control.src.tests.utils import one_robot_placement, setup_pvp
 from team_controller.src.controllers import RSimRobotController
 from rsoccer_simulator.src.ssl.envs.standard_ssl import SSLStandardEnv
 from entities.game import Game
@@ -9,48 +10,6 @@ from robot_control.src.intent import score_goal
 from motion_planning.src.pid import PID
 from team_controller.src.controllers.sim.rsim_robot_controller import PVPManager
 from team_controller.src.config.settings import TIMESTEP
-
-def setup_pvp(env: SSLStandardEnv, game: Game, n_robots_blue: int, n_robots_yellow: int):
-    pvp_manager = PVPManager(env, n_robots_blue, n_robots_yellow, game)
-    sim_robot_controller_yellow = RSimRobotController(
-        is_team_yellow=True, env=env, game_obj=game, debug=True, pvp_manager=pvp_manager
-    )
-    sim_robot_controller_blue = RSimRobotController(
-        is_team_yellow=False, env=env, game_obj=game, debug=False, pvp_manager=pvp_manager
-    )
-    pvp_manager.set_yellow_controller(sim_robot_controller_yellow)
-    pvp_manager.set_blue_controller(sim_robot_controller_blue)
-    pvp_manager.reset_env()
-
-    return sim_robot_controller_yellow, sim_robot_controller_blue, pvp_manager
-
-
-def one_robot_placement(controller: RSimRobotController, is_yellow: bool, pid_oren: PID, pid_2d: TwoDPID, invert: bool, team_robot_id: int):
-    ty = -1.5 if invert else 1.5
-    tx = 0
-
-    def one_step():
-        nonlocal tx, ty
-
-        latest_frame = game.get_my_latest_frame(my_team_is_yellow=is_yellow)
-        if latest_frame:
-            friendly_robots, enemy_robots, balls = latest_frame    
-            cx, cy, co = friendly_robots[team_robot_id]
-            print("CURRENT POS: ", cx, cy, co)
-            target_oren = math.pi + np.arctan2(
-                ty - cy, tx - cx
-            )
-            error = math.dist((tx, ty), (cx, cy))
-            print("DIST", error)
-            if error  < 0.002:
-                ty *= -1
-
-            oren =  math.pi / 2 if ty > 0 else - math.pi / 2
-            cmd = go_to_point(pid_oren, pid_2d, friendly_robots[team_robot_id], team_robot_id, (tx, ty), oren)
-            controller.add_robot_commands(cmd, team_robot_id)
-            controller.send_robot_commands()
-
-    return one_step
 
 if __name__ == "__main__":
     game = Game()
@@ -71,8 +30,8 @@ if __name__ == "__main__":
     pid_2d_b = TwoDPID(TIMESTEP, 1.5, -1.5, 3, 0.1, 0.0, num_robots=N_ROBOTS_BLUE)
 
     sim_robot_controller_yellow, sim_robot_controller_blue, pvp_manager = setup_pvp(env,  game, N_ROBOTS_BLUE, N_ROBOTS_YELLOW)
-    one_step_yellow = one_robot_placement(sim_robot_controller_yellow, True, pid_oren_y, pid_2d_y, False, TARGET_ROBOT)
-    one_step_blue = one_robot_placement(sim_robot_controller_blue, False, pid_oren_b, pid_2d_b, True, TARGET_ROBOT)
+    one_step_yellow = one_robot_placement(sim_robot_controller_yellow, True, pid_oren_y, pid_2d_y, False, TARGET_ROBOT, game)
+    one_step_blue = one_robot_placement(sim_robot_controller_blue, False, pid_oren_b, pid_2d_b, True, TARGET_ROBOT, game)
 
     try:
         while True:
