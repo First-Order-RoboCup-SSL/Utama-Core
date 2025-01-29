7a9c0a6d6ab422b8372d6d28001ba5c96a258fcc
Fred Huang <fredhuang122106@gmail.com>
2025-01-29 21:58:23 +0000

    inverse kinematics

--- team_controller/src/tests/grsim_wheel velocity_test.py
+++ team_controller/src/tests/grsim_wheel velocity_test.py
@@ -25,13 +25,29 @@ import logging
 logger = logging.getLogger(__name__)
 sim_robot_controller = GRSimRobotController(is_team_yellow=True)
 
+def inverse_kinematics(forward_vel: float, left_vel:float, angular_vel: float, ) -> List[float]:
+    """
+    Calculate the wheel velocities for a robot given the desired forward, left, and angular velocities.
+    """
+    # Example usage
+    wheel_angles = [np.deg2rad(57.3), np.deg2rad(137.03), np.deg2rad(222.97), np.deg2rad(302.7)]  # Wheel angles in radians
+
+    r = 0.178  # Distance from center to wheel in meters
+    
+    wheel_velocities = []
+    for theta in wheel_angles:
+        v_i = forward_vel * np.sin(theta) - left_vel * np.cos(theta) + angular_vel * r
+        wheel_velocities.append(v_i)
+    return wheel_velocities
+
 def packet_builder():
     out_packet = RobotControl()
+    wheel_velocities = inverse_kinematics(forward_vel=0, left_vel=10, angular_vel=0)
     command = RobotVelCommand(
-            front_right=0.0698*100,
-            front_left=0.0698*100,
-            back_right=-0.010*100,
-            back_left=-0.0732*100,
+            front_right=wheel_velocities[0],
+            back_right=wheel_velocities[1],
+            back_left=wheel_velocities[2],
+            front_left=wheel_velocities[3],
             kick=0,
             chip=0,
             dribble=0,
