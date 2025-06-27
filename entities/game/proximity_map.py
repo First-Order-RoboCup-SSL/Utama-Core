f73160d5615603e62db738ab17038e538d97f5a5
Joel <nzmjoel@gmail.com>
2025-06-27 19:16:47 +0100

    moved predictors

---
+++ entities/game/proximity_map.py
@@ -0,0 +1,19 @@
+class ProximityMap:
+    """
+    A proximity map that tracks the distance between robots and the ball.
+    """
+
+    def __init__(self):
+        self._distances = {}
+
+    def update(self, robot_key: ObjectKey, ball_key: ObjectKey, distance: float):
+        """
+        Update the distance between a robot and the ball.
+        """
+        self._distances[(robot_key, ball_key)] = distance
+
+    def get_distance(self, robot_key: ObjectKey, ball_key: ObjectKey) -> float:
+        """
+        Get the distance between a robot and the ball.
+        """
+        return self._distances.get((robot_key, ball_key), float("inf"))
--- strategy/utils/selector_utils.py
+++ strategy/utils/selector_utils.py
@@ -2,37 +2,10 @@ import py_trees
 from strategy.common.abstract_behaviour import AbstractBehaviour
 
 
-class SetWhoHasBall(AbstractBehaviour):
-    """
-    A behaviour that sets the robot ID of the robot that has the ball on the blackboard.
-    This is used to determine which robot should take actions related to the ball.
-
-    If a robot has the ball, it will set the `HasBallRobotID` key in the blackboard and return success.
-    Else if no robot has the ball, it will set `HasBallRobotID` to -1 and return failure.
-    """
-
-    def __init__(self, name="SetWhoHasBall"):
-        super().__init__(name=name)
-        self.blackboard.register_key(
-            key="HasBallRobotID", access=py_trees.common.Access.WRITE
-        )
-
-    def update(self):
-        for (
-            robot_id,
-            robot,
-        ) in self.blackboard.present_future_game.current.friendly_robots.items():
-            if robot.has_ball:
-                self.blackboard.set(name="HasBallRobotID", value=robot_id)
-                return py_trees.common.Status.SUCCESS
-        self.blackboard.set(name="HasBallRobotID", value=-1)
-        return py_trees.common.Status.FAILURE
-
-
 class HasBall(AbstractBehaviour):
     """
     A condition behaviour that checks if the robot has the ball.
-    Requires `robot_id` to be set in the blackboard.
+    Requires `robot_id` to be set in the blackboard prior.
     """
 
     def __init__(self, name="HasBall"):
