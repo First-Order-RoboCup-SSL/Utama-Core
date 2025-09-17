52dc10e16230cded23a05adb7039d5508483cef2
Fred Huang <fredhuang122106@gmail.com>
2025-09-17 13:35:19 +0100

    added blackboard variables to the visualiser

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from utama_core.replay import ReplayWriterConfig
 from utama_core.run import StrategyRunner
-from utama_core.strategy.examples.strategies.startup_strategy import StartupStrategy
+from utama_core.strategy.examples.strategies.defense_strategy import DefenceStrategy
 
 if __name__ == "__main__":
     # Set up the runner
     runner = StrategyRunner(
-        strategy=StartupStrategy(),
+        strategy=DefenceStrategy(robot_id=0),
         my_team_is_yellow=True,
         my_team_is_right=True,
         mode="rsim",
-        exp_friendly=6,
+        exp_friendly=3,
         exp_enemy=3,
         replay_writer_config=ReplayWriterConfig(replay_name="test_replay", overwrite_existing=True),
     )
