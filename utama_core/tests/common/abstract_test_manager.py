94340e53b0fc3b42e335ea952691594cdaccbc7c
Joel <nzmjoel@gmail.com>
2026-01-15 16:09:55 +0000

    improve test manager docs

--- utama_core/tests/common/abstract_test_manager.py
+++ utama_core/tests/common/abstract_test_manager.py
@@ -18,8 +18,7 @@ class TestingStatus(Enum):
 
 class AbstractTestManager(ABC):
     def __init__(self):
-        # change episode_i to current_episode_i
-        self.episode_i = 0
+        self.current_episode_number = 0
         self.my_strategy: AbstractStrategy = None
         self.opp_strategy: AbstractStrategy = None
 
@@ -28,22 +27,44 @@ class AbstractTestManager(ABC):
         self.my_strategy = my_strategy
         self.opp_strategy = opp_strategy
 
-    def update_episode_n(self, episode_i: int):
+    def update_episode_n(self, current_episode_number: int):
         """Method is used to sync test_manager on the iteration number that strategyRunner thinks it is on."""
-        self.episode_i = episode_i
+        self.current_episode_number = current_episode_number
+
+    ### START OF FUNCTIONS TO BE IMPLEMENTED FOR YOUR MANAGER ###
 
     @abstractmethod
     def reset_field(self, sim_controller: AbstractSimController, game: Game):
-        """Method is called at start of each test episode in strategyRunner.run_test Reset position of robots and ball
-        for the next strategy test."""
+        """
+        Method is called at start of each test episode in strategyRunner.run_test().
+        Use this to reset position of robots and ball for the next episode.
+        Args:
+            sim_controller (AbstractSimController): The simulation controller to manipulate robot positions.
+            game (Game): The current game state.
+        """
         ...
 
     @abstractmethod
     def eval_status(self, game: Game) -> TestingStatus:
-        """Method is called on each iteration in strategyRunner.run_test Evaluate the status of the test episode."""
+        """
+        Method is called on each iteration in strategyRunner.run_test Evaluate the status of the test episode.
+
+        Returns the current status of the test episode:
+        - TestingStatus.SUCCESS: test passed (terminate the episode with success)
+        - TestingStatus.FAILURE: test failed (terminate the episode with failure)
+        - TestingStatus.IN_PROGRESS: test still ongoing (continue running the episode)
+        """
         ...
 
     @abstractmethod
-    def get_n_episodes(self) -> int:
-        """Method is called at start of strategyRunner.run_test Get the number of episodes to run for the test."""
+    @property
+    def n_episodes(self) -> int:
+        """
+        Specify here the number of episodes the test manager should run.
+
+        Returns:
+            int: Number of episodes to run.
+        """
         ...
+
+    ### END OF FUNCTIONS TO BE IMPLEMENTED FOR YOUR MANAGER ###
