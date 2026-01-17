7cf62de69baaad2fe32fcaf5f83d164eaa89e428
Ningchaun <1311856651@qq.com>
2026-01-17 16:58:05 +0000

    Fix: fix the bug run toward enemy

--- utama_core/skills/src/defend_parameter.py
+++ utama_core/skills/src/defend_parameter.py
@@ -17,40 +17,56 @@ def defend_parameter(
     robot_id: int,
     env: Optional[SSLStandardEnv] = None,
 ):
-    shooting_enemy = game.enemy_robots[robot_id]
     defenseing_friendly = game.friendly_robots[robot_id]
+    vel = game.ball.v.to_2d()
+    if vel[0]**2 + vel[1]**2 > 0.05:
+        x1, y1 = game.ball.p.x, game.ball.p.y
+        x2, y2 = 4.5, 0.5
 
-    robot_rad = 0.09  # radius of robot in meters
-    # Calculate the perpendicular projection point from the robot to the goal line
-    x1 = shooting_enemy.p.x
-    y1 = shooting_enemy.p.y
-    x2 = 4.5
-    y2 = -0.5
-    x3 = defenseing_friendly.p.x
-    y3 = defenseing_friendly.p.y
-    t = (x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)
-    t /= (x2 - x1) ** 2 + (y2 - y1) ** 2
+        x3, y3 = defenseing_friendly.p.x, defenseing_friendly.p.y
+
+        t = ((x3 - x1) * (x2 - x1) + (y3 - y1) * (y2 - y1)) / ((x2 - x1)**2 + (y2 - y1)**2)
+        x4 = x1 + t * (x2 - x1)
+        y4 = y1 + t * (y2 - y1)
+
+        target_pos = np.array([x4, y4])
+        return go_to_point(
+                game,
+                motion_controller,
+                robot_id,
+                Vector2D(target_pos[0], target_pos[1]),
+                dribbling=True,
+            )
+        
+    shooting_enemy = game.enemy_robots[0]
+
+    robot_rad = 0.09 
+
+    x1, y1 = game.ball.p.x, game.ball.p.y
+    x2, y2 = 4.5, -0.5 
+
+    x3, y3 = defenseing_friendly.p.x, defenseing_friendly.p.y
+
+    t = ((x3 - x1) * (x2 - x1) + (y3 - y1) * (y2 - y1)) / ((x2 - x1)**2 + (y2 - y1)**2)
     x4 = x1 + t * (x2 - x1)
     y4 = y1 + t * (y2 - y1)
 
-    def normalize_vector(v: Vector2D) -> Vector2D:
-        norm = np.sqrt(v.x**2 + v.y**2)
-        if norm == 0:
-            return Vector2D(0, 0)
-        return Vector2D(v.x / norm, v.y / norm)
+    vec_to_target = np.array([x4 - x3, y4 - y3])
+    dist_to_target = np.linalg.norm(vec_to_target)
 
-    def vector_angle(v: Vector2D) -> float:
-        return np.arctan2(v.y, v.x)
+    if dist_to_target > 0:
+        vec_dir = vec_to_target / dist_to_target
+    else:
+        vec_dir = np.array([0.0, 0.0])
 
-    target_pos = Vector2D(x4, y4) - normalize_vector(Vector2D(x4 - x3, y4 - y3)) * robot_rad
-    target_oren = vector_angle(Vector2D(x3 - x1, y3 - y1))
+    target_pos = np.array([x4, y4]) - vec_dir * robot_rad
 
     target_oren = np.pi if game.my_team_is_right else 0
     return move(
         game,
         motion_controller,
         robot_id,
-        target_pos,
+        Vector2D(target_pos[0], target_pos[1]),
         target_oren,
-        True,
+        True
     )
