9a9c7e93ed7999ac315d1725c822687f01f616b2
Fred Huang <fredhuang122106@gmail.com>
2026-01-20 13:18:18 +0000

    chore: clean up

--- utama_core/strategy/examples/two_robot_placement.py
+++ utama_core/strategy/examples/two_robot_placement.py
@@ -14,7 +14,30 @@ from utama_core.strategy.common.abstract_behaviour import AbstractBehaviour
 
 # from robot_control.src.tests.utils import one_robot_placement
 from utama_core.strategy.common.abstract_strategy import AbstractStrategy
-from utama_core.strategy.common.field_behaviours import CalculateFieldCenter
+
+
+class CalculateFieldCenter(AbstractBehaviour):
+    """
+    Calculates the center of the provided field bounds and writes it to the blackboard.
+    """
+
+    def __init__(self, field_bounds: FieldBounds, output_key: str = "FieldCenter"):
+        super().__init__(name="CalculateFieldCenter")
+        self.output_key = output_key
+        self.field_bounds = field_bounds
+        self.calculated = False
+
+    def setup_(self):
+        self.blackboard.register_key(key=self.output_key, access=py_trees.common.Access.WRITE)
+
+    def update(self) -> py_trees.common.Status:
+        if self.calculated:
+            return py_trees.common.Status.SUCCESS
+
+        center = self.field_bounds.center
+        self.blackboard.set(self.output_key, center, overwrite=True)
+        self.calculated = True
+        return py_trees.common.Status.SUCCESS
 
 
 class RobotPlacementStep(AbstractBehaviour):
