e4c7eafb3bb76f6a134e5158bcc6e7cc173a32d6
Sarthak Agarwal <sarth@Sarthaks-Macbook.local>
2026-01-24 22:51:21 +0000

    updating

--- utama_core/motion_planning/src/fastpathplanning/config.py
+++ utama_core/motion_planning/src/fastpathplanning/config.py
@@ -1,9 +1,3 @@
-import logging
-from math import dist, pi
-from typing import Generator, List, Optional, Tuple, Union
-
-import numpy as np  # type: ignore
-
 from utama_core.config.physical_constants import ROBOT_RADIUS
 
 
