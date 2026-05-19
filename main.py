63c27448be4c26868d4b0569612a05a629425cfe
isaac0804 <isaacyong0804@gmail.com>
2026-05-20 00:39:34 +0100

    feat: add goal_depth to FieldDimensions and propagate to RefereeGeometry and RSim renderer

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
         strategy=StartupStrategy(),
         my_team_is_yellow=True,
         my_team_is_right=True,
-        mode="rsim",
+        mode="grsim",
         exp_friendly=2,
         exp_enemy=0,
         replay_writer_config=ReplayWriterConfig(replay_name="test_replay", overwrite_existing=True),
