d7deaf710ea60552cf279d13527f4a4e15b1e6eb
Fred Huang <fredhuang122106@gmail.com>
2026-06-02 00:43:16 +0100

    Enforce blackboard contracts in behaviour setup

--- utama_core/strategy/common/__init__.py
+++ utama_core/strategy/common/__init__.py
@@ -4,3 +4,10 @@ from utama_core.strategy.common.abstract_strategy import (
     SpaceRequirements,
 )
 from utama_core.strategy.common.base_blackboard import BaseBlackboard
+from utama_core.strategy.common.blackboard_contract import (
+    BlackboardKeySpec,
+    declared_contract,
+    register_blackboard_contract,
+    resolve_runtime_key,
+    validate_blackboard_contracts,
+)
